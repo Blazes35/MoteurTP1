ce1310d111c8dbde20ab0b3f996331419de5c8e1
Loris <loridam2@gmail.com>
2025-09-22 02:02:09 -0400

    Setup project

---
+++ Source/MoteurTP1/MoteurTP1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoteurTP1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MoteurTP1, "MoteurTP1" );
